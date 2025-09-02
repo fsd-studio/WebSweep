33ab699f0f500789ec7fb07947963e8f63b6e5dd
Sen-Vmh <r0982669@student.thomasmore.be>
2025-09-02 20:38:39 +0200

    fixed re-routing

--- i18n.js
+++ i18n.js
@@ -2,12 +2,17 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-import deCommon from './public/locales/de/common.json';
-import deNav from './public/locales/de/nav.json';
-import enCommon from './public/locales/en/common.json';
-import enNav from './public/locales/en/nav.json';
-import huCommon from './public/locales/hu/common.json';
-import huNav from './public/locales/hu/nav.json';
+// Use correct relative paths
+import deCommon from './locales/de/common.json';
+import deNav from './locales/de/nav.json';
+import enCommon from './locales/en/common.json';
+import enNav from './locales/en/nav.json';
+import huCommon from './locales/hu/common.json';
+import huNav from './locales/hu/nav.json';
+
+console.log('EN Common:', enCommon);
+console.log('HU Common:', huCommon);
+console.log('DE Common:', deNav);
 
 const resources = {
   en: {
@@ -25,19 +30,18 @@ const resources = {
 };
 
 i18n
-  .use(LanguageDetector) 
-  .use(initReactI18next) 
+  .use(LanguageDetector)
+  .use(initReactI18next)
   .init({
-    resources, // Use bundled resources instead of backend
-    supportedLngs: ['en', 'de', 'hu'], // Supported languages
-    fallbackLng: 'en',                 // Fallback language
-    debug: false,
+    resources,
+    supportedLngs: ['en', 'de', 'hu'],
+    fallbackLng: 'en',
+    debug: true, // Set to true temporarily to see error messages
     interpolation: {
-      escapeValue: false, 
+      escapeValue: false,
     },
-
     ns: ['common', 'nav'],
-    defaultNS: 'common' // default namespace
+    defaultNS: 'common'
   });
 
 export default i18n;
